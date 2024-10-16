e6daa49cf4a07beb025f0f1c825993bd9322d0fa
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2024-10-16 14:39:34 +0300

    clean code

--- Hardware Triggers/remote.py
+++ Hardware Triggers/remote.py
@@ -2,36 +2,36 @@ import subprocess
 import serial
 import time
 
-new_threshold = 75.0  
-sketchPath = "sketch_oct14b/sketch_oct14b.ino"
+THRESHOLD = 75.0  
+SKETCH = "sketch_oct14b/sketch_oct14b.ino"
+PORT = "/dev/cu.usbmodem11301"
 
-def compileArduino(sketchPath):
+
+def compileArduino(SKETCH):
     try:
-        compileCommand = f"arduino-cli compile --fqbn arduino:avr:uno {sketchPath}"
+        compileCommand = f"arduino-cli compile --fqbn arduino:avr:uno {SKETCH}"
         subprocess.run(compileCommand, check=True, shell=True)
         print("Compilation successful!")
+        
     except subprocess.CalledProcessError as e:
         print(f"Error during compilation: {e}")
 
-def uploadArduino(sketchPath, port):
+def uploadArduino(SKETCH, port):
     try:
-        uploadCommand = f"arduino-cli upload -p {port} --fqbn arduino:avr:uno {sketchPath}"
+        uploadCommand = f"arduino-cli upload -p {port} --fqbn arduino:avr:uno {SKETCH}"
         subprocess.run(uploadCommand, check=True, shell=True)
         print("Upload successful!")
+        
     except subprocess.CalledProcessError as e:
         print(f"Error during upload: {e}")
 
 def sendThresholdToArduino(port, threshold):
     arduino = serial.Serial(port, 9600, timeout=1)
-    
     time.sleep(10)
-    
-    threshold_str = str(threshold) + "\n"  # Add newline to mark end of input
-    arduino.write(threshold_str.encode())
+    arduino.write( f'{str(threshold)}\n'.encode())
     arduino.close()
-arduinoPort = "/dev/cu.usbmodem11301"
 
-compileArduino(sketchPath)
-uploadArduino(sketchPath, arduinoPort)
-sendThresholdToArduino(arduinoPort, new_threshold)
+compileArduino(SKETCH)
+uploadArduino(SKETCH, PORT)
+sendThresholdToArduino(PORT, THRESHOLD)
 
